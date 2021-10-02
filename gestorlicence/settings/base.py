6e721e4a332c56b1a2f687eb57841d7308ed26c2
livington <livingtonllopezc124@gmail.com>
2021-10-01 19:28:07 -0500

    LISTO PARA DEPLOY

--- gestorlicence/settings/base.py
+++ gestorlicence/settings/base.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os # needed by code below
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django_heroku #importamos el heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -111,7 +113,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Activa Django-Heroku.
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
